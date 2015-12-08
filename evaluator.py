41f41237ea950206caecc2e7ab6fd254cbe8cc55
annaywlee <annaywlee@gmail.com>
2015-12-08 17:18:10 -0500

    updated evaluate() to better handle non 1-1 matches b/w called and true mutations

--- evaluator.py
+++ evaluator.py
@@ -152,16 +152,19 @@ def prefix(rec, usechr):
     
     
 def evaluate(submission, truth, vtype='SNV', ignorechroms=None, truthmask=True):
-    ''' return stats on sensitivity, specificity, balanced accuracy '''
+    ''' return TP, FP and FN counts '''
 
     assert vtype in ('SNV', 'SV', 'INDEL')
     subvcfh = vcf.Reader(filename=submission)
     truvcfh = vcf.Reader(filename=truth)
 
-    tpcount = 0
-    fpcount = 0
-    subrecs = 0
-    trurecs = 0
+    tpcount = 0 # counts TPs that are not masked
+    fpcount = 0 # counts FPs that not masked
+    tpcountmasked = 0 # counts masked TPs
+    fpcountmasked = 0 # counts masked FPs
+    subrecs = 0 # counts all predicted variant records
+    subuncount = 0 # counts predicted variant records that are not counted as TPs/FPs
+    trunotmasked = 0 # counts true variants that are not masked
 
     truchroms = {}
 
@@ -174,84 +177,128 @@ def evaluate(submission, truth, vtype='SNV', ignorechroms=None, truthmask=True):
     ''' count records in truth vcf, track contigs/chromosomes '''
     for trurec in trulist:
         if relevant(trurec, vtype, ignorechroms):
-            trurecs += 1
             truchroms[trurec.CHROM] = True
+            if not mask(trurec, truvcfh, truchroms, active=truthmask):
+                trunotmasked += 1
+    
+    # sanity check
+    if trunotmasked == 0:
+        raise Exception("No unmasked records found in truth file!\n")
 
-    ''' subtract masked variants from truth '''
-    trumasked = 0
-    submasked = 0
 
-    for trurec in trulist:
-        if relevant(trurec, vtype, ignorechroms) and mask(trurec, truvcfh, truchroms, active=truthmask):
-            trurecs -= 1
-            trumasked += 1
-
-    used_truth = {} # keep track of 'truth' sites used, they should only be usable once
-    used_bnd_mates = {} # if submitters use MATEID in their BND calls we can 'tie' them together
+    '''
+    keep track of 'truth' sites used, they should only be usable once
+    if toCount=True, the true variant will be counted; if False, it will not
+    '''
+    used_truth = {}
+    
+    '''
+    if submitters use MATEID in their BND calls we can 'tie' them together,
+    indexed by one mate, contains info on other mate in pair
+    '''
+    used_bnd_mates = {}
 
     ''' parse submission vcf, compare to truth '''
     for subrec in subvcfh:
         subrec = prefix(subrec, usechr)
-        if relevant(subrec, vtype, ignorechroms) and not mask(subrec, truvcfh, truchroms, active=truthmask):
-            if passfilter(subrec):
-                if subrec.is_snp and vtype == 'SNV':
-                    subrecs += 1
-                if subrec.is_sv and vtype == 'SV':
-                    subrecs += 1
-                if subrec.is_indel and vtype == 'INDEL':
-                    subrecs += 1
-
-            matched = False
-            SV_BND_multimatch = False 
+        if relevant(subrec, vtype, ignorechroms) and passfilter(subrec):
+            subrecs += 1
+            
+            matched = 'UN'
 
             startpos, endpos = subrec.start, subrec.end
 
             if vtype == 'SV' and subrec.is_sv:
                 startpos, endpos = expand_sv_ends(subrec, useCIs=False)
+            
+            sub_is_masked = mask(subrec, truvcfh, truchroms, active=truthmask)
+            
+            if subrec.CHROM in truchroms:
+                truoverlaplist = [trurec for trurec in truvcfh.fetch(subrec.CHROM, startpos, end=endpos)]
+                for trurec in truoverlaplist:
+                    if relevant(trurec, vtype, ignorechroms) and match(subrec, trurec, vtype=vtype):
+                        # subrec matches a true variant
+                        tru_is_masked = mask(trurec, truvcfh, truchroms, active=truthmask)
+                        
+                        if not sub_is_masked and not tru_is_masked:
+                            if str(trurec) not in used_truth and matched == 'TP':
+                                # subrec already matches another true variant
+                                used_truth[str(trurec)] = { 'toCount': False, 'masked': tru_is_masked }
+                            
+                            elif matched != 'TP' and (str(trurec) not in used_truth or not used_truth[str(trurec)]['toCount']):
+                                # subrec matches an unused true variant
+                                matched = 'TP'
+                                used_truth[str(trurec)] = { 'toCount': True, 'masked': tru_is_masked }
+                        
+                        elif str(trurec) not in used_truth:
+                            # subrec matches an unused true variant but at least one is masked
+                            used_truth[str(trurec)] = { 'toCount': False, 'masked': tru_is_masked }
+            
+                        if matched != 'TP':
+                            '''
+                            matched a true variant that was already counted
+                            and/or has a different mask status/both are masked
+                            only note if a true variant wasn't already IDed for subrecj
+                            '''
+                            matched = 'T'
+
+            if matched == 'TP' or matched == 'T':
+                if subrec.ID in used_bnd_mates and not used_bnd_mates[subrec.ID]['positive']:
+                    # BND mate call was a false positive, remove conflict
+                    if used_bnd_mates[subrec.ID]['masked']:
+                        fpcountmasked -= 1
+                    else:
+                        fpcount -= 1
+                    subuncount += 1
+                
+                elif subrec.INFO.get('MATEID'):
+                    # keep track of the mate info
+                    if isinstance(subrec.INFO.get('MATEID'), list):
+                        mateID = subrec.INFO.get('MATEID')[0]
+                    else:
+                        mateID = subrec.INFO.get('MATEID')
+                    used_bnd_mates[mateID] = { 'positive': True, 'masked': sub_is_masked }
+            
+            elif matched == 'UN' and subrec.ID not in used_bnd_mates:
+                # subrec does not match a true variant and it is not tied to a previous match through a mate
+                matched = 'FP'
+                
+                if sub_is_masked:
+                    fpcountmasked += 1
+                else:
+                    fpcount += 1
+                    
                 if subrec.INFO.get('MATEID'):
-                    used_bnd_mates[subrec.INFO.get('MATEID')[0]] = True
-            try:
-                if subrec.CHROM in truchroms and passfilter(subrec):
-                    for trurec in truvcfh.fetch(subrec.CHROM, startpos, end=endpos):
-                        # if using BND notation, don't penalize multiple BND records matching one truth interval
-                        if str(trurec) in used_truth:
-                            if vtype == 'SV' and subrec.INFO.get('SVTYPE') and subrec.INFO.get('SVTYPE') == 'BND':
-                                SV_BND_multimatch = True
-
-                        if match(subrec, trurec, vtype=vtype) and str(trurec) not in used_truth:
-                            matched = True
-                            used_truth[str(trurec)] = True
-
-                if not matched and subrec.ID in used_bnd_mates:
-                    SV_BND_multimatch = True
-
-            except ValueError as e:
-                sys.stderr.write("Warning: " + str(e) + "\n")
-
-            if matched:
-                tpcount += 1
-            else:
-                if passfilter(subrec): 
-                    if not SV_BND_multimatch: # don't penalize BND multi-matches to truth intervals
-                        fpcount += 1 
-
-        else:
-            if relevant(subrec, vtype, ignorechroms):
-                submasked += 1
-
-    print "tpcount, fpcount, subrecs, submasked, trurecs, trumasked:"
-    print tpcount, fpcount, subrecs, submasked, trurecs, trumasked
+                    # keep track of the mate info
+                    if isinstance(subrec.INFO.get('MATEID'), list):
+                        mateID = subrec.INFO.get('MATEID')[0]
+                    else:
+                        mateID = subrec.INFO.get('MATEID')
+                    used_bnd_mates[mateID] = { 'positive': False, 'masked': sub_is_masked }
+                
+            if matched == 'TP':
+                if sub_is_masked:
+                    tpcountmasked += 1
+                else:
+                    tpcount += 1
+            elif matched == 'UN' or matched == 'T':
+                subuncount += 1
 
     # sanity checks
-    if trurecs == 0:
-        raise Exception("No unmasked records found in truth file!\n")
-
+    assert (tpcount + fpcount + tpcountmasked + fpcountmasked + subuncount == subrecs)
+    
     if subrecs == 0:
-        raise Exception("No unmasked variants in submission! Are you sure you selected the correct variant type (SNV/INDEL/SV)?\n")
+        raise Exception("No filter-passing variants in submission! Are you sure you selected the correct variant type (SNV/INDEL/SV)?\n")
 
+    # count the true variants that should not be counted as TPs/FNs
+    truuncountnotmasked = 0
+    for k, v in used_truth.iteritems():
+        if not v['toCount'] and not v['masked']:
+            truuncountnotmasked += 1
+    
     result = { 'tp' : float(tpcount),
                'fp' : float(fpcount),
-               'fn' : float(trurecs) - float(tpcount)}
+               'fn' : float(trunotmasked - tpcount - truuncountnotmasked) }
     
     return result
 
